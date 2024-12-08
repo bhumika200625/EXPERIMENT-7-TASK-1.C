fa8a3f8ccf966f3e04e65ee16b535cfd2f79b236
Bhumika Chauhan <bhumika@eng.rizvi.edu.in>
2024-12-08 11:16:18 +0530

    Update Experiment 7 task 1.c

--- Experiment 7 task 1.c
+++ Experiment 7 task 1.c
@@ -1,47 +1,19 @@
-#include <stdio.h>
-
+#include<stdio.h>
 int main(){
-    int rows,cols,i,j;
-    
-    
-    printf("\t\t*** Add Arrays***\n\n");
-    
-    printf("Enter Array Size (row,column):");
-    scanf("%d,%d",&rows,&cols);
-    int arr_a[rows][cols],arr_b[rows][cols],sum[rows][cols];
-    printf("Enter First Array Elements:\n");
-    for(i=0;i< rows;i++){
-        for(j=0;j<cols;j++){
-            printf("%d,%d:",i,j);
-            scanf("%d",&arr_a[i][j]);
-            
-        }
-    }
-    printf("\n");
-    printf("Enter Second Array Elements:\n");
-    for(i=0;i< rows; i++){
-        for(j=0;j<cols;j++){
-            printf("%d,%d:",i,j);
-            scanf("%d",&arr_b[i][j]);
-            
-        }
- }
-    printf("\n");
-    
-    printf("Sum of both Arrays;\n");
-    for(i=0;i<rows;i++){
-        for(j=0;j<cols;j++){
-            sum[i][j]+arr_b[i][j];
-            printf("%d",sum[i][j]);
-            
-        }
-        
-        printf("\n");
-    }
-    
-    
-    
-    return 0;
-    
-    
-}    
+int size,i,largest=0;
+printf("\t\t *** Array Largest Number Finder *** \n\n");
+printf("Enter Array Size:");
+scanf("%d", &size);
+int numbers[size];
+printf("Enter Array Elements:\n");
+for(i = 0; i < size; i++){
+printf("%d: ",i);
+scanf("%d", &numbers[i]);
+if(numbers[i] > largest){
+largest = numbers[i];
+}
+}
+printf("\nLargest Number: %d", largest);
+return 0;
+}
+
